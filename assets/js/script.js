192019827af0efb2680a0a67ca061d8ead38efc9
hassenH <hadhri-h@live.fr>
2019-02-09 16:08:36 +0100

    Ajout Exercice1

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#clickMe').click(function() { // Lors du clic avec l'evenement .click(), une boite de dialogue va s'ouvrir
+    alert('Bonjour'); // Boite de dialogue
+  });
+});
